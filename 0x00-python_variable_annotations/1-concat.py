d2ba66436611be79f9f33c0e2da56d15efcd1b69
EstherKiguru <esipraisewambui@gmail.com>
2024-07-05 00:45:38 +0300

    concatenate annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for concatenate annotation"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes strl1 & strl2 as arguments
+    returns: concatenated string
+    """
+    return str1 + str2
