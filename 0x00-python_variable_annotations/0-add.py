72d48c358438d57e1678e3f5e6b640c90863a750
EstherKiguru <esipraisewambui@gmail.com>
2024-07-05 00:38:04 +0300

    add annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bn/env/python3
+"""Module for add annotations"""
+
+
+def add(a: float, b: float) -> float:
+    """takes float a and float b
+    returns: sum as a float
+    """
+    return a + b
