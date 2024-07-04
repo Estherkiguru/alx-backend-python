159a012c30bca311104c1e26197ec874645b6c00
EstherKiguru <esipraisewambui@gmail.com>
2024-07-05 00:58:33 +0300

    to_str annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/emv python3
+"""Module for type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """takes n as argument
+    returns: string representaton of float
+    """
+    return str(n)
