57cb055c9ea8600ad3b74cefce78b1909bdb5c8f
EstherKiguru <esipraisewambui@gmail.com>
2024-07-10 01:31:56 +0300

    random numbers

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module for a coroutine called async_comprehension that takes no arguments."""
+"""Module for coroutine async_comprehension that takes no arguments."""
 
 from typing import List
 from importlib import import_module as using
