441df2ed0e31dfc16be593beafa91b63fe13b1cd
EstherKiguru <esipraisewambui@gmail.com>
2024-07-29 23:05:49 +0300

    unittest for client.GithubOrgClient class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -22,4 +22,14 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class = GithubOrgClient(input)
         test_class.org()
         mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+    def test_public_repos_url(self):
+        """ Test case for _public_repos_url property
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
 
